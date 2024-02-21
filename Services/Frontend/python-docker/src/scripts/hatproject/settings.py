e35a6e368e517eb64c1fb89f2add7503c5239d66
Mikhail Popov <astat.popov@yandex.ru>
2024-02-21 17:32:53 +0300

    delete frontend and env

--- Services/Frontend/python-docker/src/scripts/hatproject/settings.py
+++ Services/Frontend/python-docker/src/scripts/hatproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'transportMap.apps.TransportmapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,16 +75,16 @@ WSGI_APPLICATION = 'hatproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'djangobackenddb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangobackenddb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- Services/Frontend/python-docker/src/scripts/transportMap/models.py
+++ Services/Frontend/python-docker/src/scripts/transportMap/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Stop(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=200)
