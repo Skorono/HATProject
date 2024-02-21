9886c5d3b59b9386ffc7a517dc747f92c183882c
Skorono <58297559+Skorono@users.noreply.github.com>
2024-02-21 17:33:44 +0300

    Delete Services/Frontend/python-docker directory
    delete frontend folder

--- Services/Frontend/python-docker/src/scripts/transportMap/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransportmapConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transportMap'
--- Services/Frontend/python-docker/src/scripts/transportMap/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Stop(models.Model):
-    id = models.IntegerField(auto_created=True, primary_key=True)
-    name = models.CharField(max_length=200)
